eda8300252c1facc678da1bd4e22339302ce99fd
ENOCH EKOW ENU <30849211+ekow1@users.noreply.github.com>
2022-08-20 02:28:15 +0000

    Update Allusers.jsx

--- src/components/Allusers/Allusers.jsx
+++ src/components/Allusers/Allusers.jsx
@@ -32,23 +32,23 @@ function Allusers() {
         <MDBCol size='3'>
           <MDBTabs className='flex-column text-center fw-bold fs-4'>
             <MDBTabsItem>
-              <MDBTabsLink onClick={() => handleVerticalClick('tab1')} active={verticalActive === 'tab1'} className='  fs-5'>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab1')} active={verticalActive === 'tab1'} className='  fw-bold'>
                 Fetch F-Component
               </MDBTabsLink>
             </MDBTabsItem>
             <MDBTabsItem>
-              <MDBTabsLink onClick={() => handleVerticalClick('tab2')} active={verticalActive === 'tab2'} className='  fs-5'>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab2')} active={verticalActive === 'tab2'} className='  fw-bold'>
               Fetch C-Component
               </MDBTabsLink>
             </MDBTabsItem>
             <MDBTabsItem>
-              <MDBTabsLink onClick={() => handleVerticalClick('tab3')} active={verticalActive === 'tab3'} className='  fs-5'
+              <MDBTabsLink onClick={() => handleVerticalClick('tab3')} active={verticalActive === 'tab3'} className='  fw-bold'
 >
                Axios F-Component
               </MDBTabsLink>
             </MDBTabsItem>
             <MDBTabsItem>
-              <MDBTabsLink onClick={() => handleVerticalClick('tab4')} active={verticalActive === 'tab4'} className='  fs-5'
+              <MDBTabsLink onClick={() => handleVerticalClick('tab4')} active={verticalActive === 'tab4'} className='  fw-bold'
 >
                 Axios C-Component
               </MDBTabsLink>
